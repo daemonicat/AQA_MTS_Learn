5a364e3e890dee83713c83db372763e0d60ab76e
Dmitry Kuzmin <dima62131@gmail.com>
2023-11-25 14:05:50 +0300

    first classwork

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,8 +4,41 @@
     {
         public static void Main(string[] args)
         {
+            int[] array = { -4, 8, 10, 2, 12, 0, 5, 6 };
+
+            PrintArray(array);
+            Console.WriteLine();
+
+            Array.Reverse(array);
+            PrintArray(array);
             
+            Console.WriteLine();
+
+            int sum1 = CalculateSum(1, 2, 3, 4, 5);
+            int sum2 = CalculateSum(); // Пустой вызов
+
+            Console.WriteLine("Sum 1: " + sum1);
+            Console.WriteLine("Sum 2: " + sum2);
+        }
+
+        private static void PrintArray(int[] inputArray)
+        {
+            foreach (var item in inputArray)
+            {
+                Console.Write($"{item} \t");
+            }
+        }
+
+        private static int CalculateSum(params int[] numbers)
+        {
+            var sum = 0;
+
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return sum;
         }
     }
 }
-
