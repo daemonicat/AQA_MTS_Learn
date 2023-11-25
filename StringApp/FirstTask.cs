e3733a43632a0e07a31f3596e0d8c7e787b7177f
Dmitry Kuzmin <dima62131@gmail.com>
2023-11-25 15:49:15 +0300

    added FirstTaskOutput.txt as example

--- StringApp/FirstTask.cs
+++ StringApp/FirstTask.cs
@@ -6,13 +6,13 @@ namespace StringApp
     {
         public static void TestToTesting()
         {
-            string? inputString = File.ReadAllText(@"..\..\..\FirstTaskIOFiles\FirstTaskInput.txt");
+            string? inputString = File.ReadAllText(@"../../../FirstTaskIOFiles/FirstTaskInput.txt");
 
             inputString = inputString.Replace("test", "testing");
             inputString = Regex.Replace(inputString, "[0-9]", "");
 
-            if (!File.Exists(@"..\..\..\FirstTaskIOFiles\FirstTaskOutput.txt"))
-                File.WriteAllText(@"..\..\..\FirstTaskIOFiles\FirstTaskOutput.txt", inputString);
+            if (!File.Exists(@"../../../FirstTaskIOFiles/FirstTaskOutput.txt"))
+                File.WriteAllText(@"../../../FirstTaskIOFiles/FirstTaskOutput.txt", inputString);
         }
     }
 }
