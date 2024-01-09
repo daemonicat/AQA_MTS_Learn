f93a92e76a631ec8c9c16889811a9fef669dd972
Dmitry Kuzmin <dima62131@gmail.com>
2024-01-09 18:11:21 +0300

    Events

---
+++ Event/ClientDelegateProgram.cs
@@ -0,0 +1,21 @@
+﻿using Event;
+
+class ClientDelegateProgram
+{
+    public delegate int CalculateDiscount(ClientType clientType);
+
+    public CalculateDiscount CalculateDiscountForClient { get; set; }
+
+    private void CalculateDiscounts(IEnumerable<Client> clients)
+    {
+        foreach (var client in clients)
+        {
+            client.Discount = CalculateDiscountForClient(client.Type);
+        }
+    }
+    
+    static void Main2()
+    {
+    }
+}
+ 
---
+++ Event/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public delegate void Handler(string alpha, string beta);
+
+    public static event Handler Stuck;
+    //public static Handler Stuck;
+
+    static void Main1()
+    {
+        Stuck += ShowMessage;
+
+        Stuck("Alex", "Trostyanko");
+    }
+
+    private static void ShowMessage(string firstname, string lastname) =>
+        Console.WriteLine($"{firstname} {lastname} Have fun!");
+}
