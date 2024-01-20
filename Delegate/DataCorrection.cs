9a409290dc72efa519e52841ddc5815b3830def3
Dmitry Kuzmin <dima62131@gmail.com>
2024-01-20 12:43:29 +0300

    Init

--- Delegate/DataCorrection.cs
+++
@@ -1,9 +0,0 @@
-namespace Delegate;
-
-public class DataCorrection
-{
-    public static void UpdateData(int price)
-    {
-        Console.WriteLine($"Data was updated according to price: {price}");
-    }
-}
--- Delegate/OperationType.cs
+++
@@ -1,6 +0,0 @@
-namespace Delegate;
-
-public enum OperationType
-{
-    Add, Subtract, Multiply
-}
